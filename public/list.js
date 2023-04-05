7dd79522b2aeb2812daf1193d1d0a1feb40e020e
Wendl <kennedyw@tmctrans.com>
2023-04-05 09:59:50 -0500

    adding files

---
+++ public/list.js
@@ -0,0 +1,66 @@
+
+var getHeaderElement = document.getElementById("headerThree");
+var itemToRemove; 
+
+window.onload = async function() {
+    var responseVar =  "";
+    await fetch('http://localhost:3000/get')
+    .then(response => {
+        return response.json();
+    })
+    .then(resp => {
+
+        var items = resp.items;
+        for (i = 0; i < items.length; ++i)
+        {
+            var listInputString = '<li tag = "' + items[i].item + '" > ' + items[i].item +  ' </li>';
+            list.innerHTML += listInputString;
+
+            itemToRemove = items[i].item;
+            document.getElementsByTagName(items[i].item).addEventListener("click", removeFromList);
+        }
+    });
+}
+
+var textInput = document.getElementById("todoItemInput")
+var list = document.getElementById("list")
+document.getElementById("removeButton").addEventListener("click", removeFromList);
+
+function addToList() {
+    console.log(textInput.value)
+    fetch('http://localhost:3000/add', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "item": textInput.value})
+})
+
+    var listInputString = '<li tag = "' + textInput.value + '" > ' + textInput.value +  ' </li>';
+    list.innerHTML += listInputString;
+    document.getElementsByTag(textInput.value).addEventListener("click", function(){
+        removeFromList(textInput.value);
+    });
+  
+  textInput.value = "";
+}
+
+function removeFromList(param) {
+    fetch('http://localhost:3000/delete', {
+    method: 'DELETE',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "item": param})
+})
+    var item = document.getElementById(param);
+    // list.removeChild(item);
+}
+
+textInput.addEventListener('keyup', (event) => {
+    if(event.which === 13) {
+        addToList(); 
+    }
+}); 
---
+++ server.js
@@ -0,0 +1,128 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+const fs = require("fs"); 
+
+app.use(express.static('public'))
+app.use(express.json());
+
+app.use(cors({
+  origin: '*'
+}))
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/todo.html');
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get("/messages", (req, res) => {
+  res.send("Hello");
+});
+
+/* 
+  Changed from '/add' to '/'
+  Trying to fix the Cannot GET / error - lol did not work, Professor Nick help pls
+*/
+app.post('/add', (req, res) => {
+  var items;
+  var dataHolder;
+  var itemsAsf = {
+    item : req.body.item
+  };
+
+  fs.readFile("database.json", (error, data) => {
+
+    if(error) {
+      console.error(error); 
+
+      throw error;
+    }
+
+    items = JSON.parse(data).items;
+    items.push(itemsAsf);
+
+    dataHolder = {
+      items: items
+    }
+
+    data = JSON.stringify(dataHolder)
+    
+    fs.writeFileSync("database.json", data, (error) => {
+  
+      if(error) {
+        console.error(error); 
+  
+        throw error;
+      }
+    })
+  })
+
+  res.send();
+
+})
+
+app.get('/get', (req, res) => {
+  var getData; 
+
+  fs.readFile("database.json", (error, data) => {
+
+    getData = JSON.parse(data); 
+
+    if(error) {
+      console.error(error); 
+
+      throw error;
+    }
+
+    // res.send('Hello World');
+
+    res.send(JSON.stringify(getData)); 
+
+  })
+
+})
+
+app.delete('/delete', (req, res) => {
+  var items;
+  var dataHolder;
+  var itemsAsf = {
+    item : req.body.item
+  };
+
+  fs.readFile("database.json", (error, data) => {
+
+    if(error) {
+      console.error(error); 
+
+      throw error;
+    }
+
+    items = JSON.parse(data).items;
+    items = items.filter(i => i.item !== itemsAsf.item);
+    console.log(itemsAsf);
+    console.log(items);
+
+    dataHolder = {
+      items: items
+    }
+
+    data = JSON.stringify(dataHolder)
+    
+    fs.writeFileSync("database.json", data, (error) => {
+  
+      if(error) {
+        console.error(error); 
+  
+        throw error;
+      }
+    })
+  })
+
+  res.send();
+})
+
